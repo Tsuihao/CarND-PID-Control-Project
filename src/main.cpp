b29783af42b6440acda45ba46b9c8b98d91dee99
Travis <>
2018-10-06 11:59:53 +0200

    Fine tuning

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.085, 0.005, 0.05);
+  pid.Init(0.08, 1E-5, 0.8);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
